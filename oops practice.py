2a7b23f4f06921c92465c248c5d4a645bdf0f909
naidu <appalanaidukottakotla30@gmail.com>
2024-06-10 10:01:02 +0530

    my first project

---
+++ oops practice.py
@@ -0,0 +1,19 @@
+'''
+class mango:
+ x=5
+ y=9
+p1=mango()
+print(p1.y,p1.x)
+'''
+class student:
+    def __init__(self,name,age,dob):
+        self.name=name
+        self.age=age
+        self.dob=dob
+   
+s1=student('naidu',26,1)
+s2=student('venk',24,1-5-2008)
+print(s2.dob)
+
+
+
